21fd535d109c400da19146073c138e2923c9846c
nittashiori <nitta@impv.co.jp>
2021-01-21 17:53:10 +0900

    チュートリアル反映

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,17 @@
-import * as test from './common/_test';
-import * as test2 from './common/_test2';
+import Cursor from './_cursor';
+import { preloader } from './_preloader';
+import LocomotiveScroll from 'locomotive-scroll';
+import Menu from './_menu';
 
-test.init();
-test2.init();
+const menuEl = document.querySelector('.menu');
+
+preloader('.menu__item').then(() => {
+    // initialize the smooth scroll
+    const scroll = new LocomotiveScroll({ el: menuEl, smooth: true });
+
+    // initialize custom cursor
+    const cursor = new Cursor(document.querySelector('.cursor'));
+
+    // initialize menu
+    new Menu(menuEl);
+});
